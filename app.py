0807727111985d5d4be5b314900501ca27d2773b
mmpatterson <mmpatterson94@gmail.com>
2021-07-02 13:28:51 -0400

    Year and Year with column filter endpoints built

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import json
+from collections import OrderedDict
+
+app = Flask(__name__)
+
+@app.route("/api/v1.0/play-by-play/<year>", methods = ['GET', 'POST'])
+def play_by_play(year):
+    """Fetch nflfastR gameplay data for the given year."""
+    if request.method == 'GET':
+        year_df = pd.read_csv(f'https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=true', compression = 'gzip')
+        year_dict = year_df.to_dict(orient="records")
+        return jsonify(year_dict)
+    elif request.method == 'POST':
+        # Column must be in list form
+        columns = request.json['columns']
+        year_df = pd.read_csv(f'https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=true', compression = 'gzip', usecols=columns)
+        year_dict = year_df.to_dict(orient="records")
+        return jsonify(year_dict)
+if __name__ == "__main__":
+    app.run(debug=True)
