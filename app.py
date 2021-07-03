afa6d83b1ea0190fd07c4d971ae266d57ebbab6d
mmpatterson <mmpatterson94@gmail.com>
2021-07-02 22:42:11 -0400

    Added orient json option

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return "<h1>Welcome to our server !!</h1>"
     
 @app.route("/api/v1.0/play-by-play/<year>", methods = ['GET', 'POST'])
-def play_by_play(year):
+def play_by_play(year, orient = 'records'):
     """Fetch nflfastR gameplay data for the given year."""
     if request.method == 'GET':
         year_df = pd.read_csv(f'https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=true', compression = 'gzip')
@@ -16,8 +16,12 @@ def play_by_play(year):
     elif request.method == 'POST':
         # Column must be in list form
         columns = request.json['columns']
+        try:
+            orient = request.json['orient']
+        else:
+            orient = orient
         year_df = pd.read_csv(f'https://github.com/nflverse/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=true', compression = 'gzip', usecols=columns)
-        year_dict = year_df.to_dict(orient="records")
+        year_dict = year_df.to_dict(orient=orient)
         return jsonify(year_dict)
 if __name__ == "__main__":
     app.run(debug=True)
